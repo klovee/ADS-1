130b5c7cf16d6a9770c371eed3ef7a97bea28513
klovee <79109849+klovee@users.noreply.github.com>
2021-03-29 15:42:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,29 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int s = 0; int count = 0; int k = 0;
+  while (true) {
+    s = (size + k) / 2;
+    if (value < arr[s]) {
+      size = s - 1;
+    } else if (value > arr[s]) {
+      k = s + 1;
+    } else {
+      while (arr[s] == value) {
+        s--;
+      }
+      s++;
+      while (arr[s] == value) {
+        count++;
+        s++;
+      }
+      break;
+    }
+    if (k > size) {
+      break;
+    }
+  }
+  return count;
+} 
+return 0; // если ничего не найдено
 }
